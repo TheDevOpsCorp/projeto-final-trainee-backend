7315b9cdf4f51798a93d22d8716071a7621cf9f6
ItzTas <talessabini2@gmail.com>
2024-08-13 19:55:15 -0400

    chore: metodo login implementado

---
+++ src/controllers/users_controller.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ */
+
+export default {
+  /**
+   * @param {Request} req
+   * @param {Response} res
+   * */
+  async login(req, res) {
+    const { password, username } = req.body;
+    if (!password || !username) {
+      return res.json({
+        error: 'password not provided',
+      });
+    }
+  },
+};
