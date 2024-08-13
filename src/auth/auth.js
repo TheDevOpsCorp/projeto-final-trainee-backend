9e9db76e2f98525168f4ae524b77d3e5c017faf4
ItzTas <talessabini2@gmail.com>
2024-08-13 19:48:27 -0400

    chore: função de assinar o token criada

---
+++ src/auth/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const dafaultExpirationDate = '4hr';
+
+const JWT_SECRET = `${process.env['JWT_SECRET']}`;
+
+/**
+ *
+ * @param {Object.<any, any>} payload
+ * @param {jwt.SignOptions} [options={ expiresIn: defaultTokenExpiration }]
+ * @returns {String}
+ */
+function signJWT(payload, options = {}) {
+  options.expiresIn = options.expiresIn || dafaultExpirationDate;
+  options.subject = payload.id;
+  payload.iat = Math.floor(Date.now() / 1000) - 30;
+  return jwt.sign(payload, JWT_SECRET, options);
+}
+
+export { signJWT };
