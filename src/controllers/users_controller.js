a6d176936246a02df2a3b8721e3b092a3b3d566c
ItzTas <talessabini2@gmail.com>
2024-08-13 23:20:16 -0400

    chore: methodo de login começado

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -3,20 +3,39 @@
  * @typedef {import('express').Response} Response
  */
 
+import {
+    ErrorNotFound,
+    getUsersbyUserName,
+} from '../database/users_queries.js';
+
 export default {
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   * */
+    /**
+     * @param {Request} req
+     * @param {Response} res
+     * */
 
-  async login(req, res) {
-    
-    const { password, username } = req.body;
+    async login(req, res) {
+        const { password, user_name } = req.body;
 
-    if (!password || !username) {
-      return res.json({
-        error: 'password not provided',
-      });
-    }
-  },
+        if (!password || !user_name) {
+            return res.status(400).json({
+                error: 'password or user name not provided',
+            });
+        }
+
+        let user;
+        try {
+            user = await getUsersbyUserName(user_name);
+        } catch (err) {
+            if (err instanceof ErrorNotFound) {
+                return res.status(404).json({
+                    error: 'user with given name does not exist',
+                });
+            }
+            return res.status(500).json({
+                error: 'could not get user',
+                error_infos: err,
+            });
+        }
+    },
 };
