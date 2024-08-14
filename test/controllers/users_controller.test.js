d438a5186161e32a8827742d575eb5f96306e489
ItzTas <talessabini2@gmail.com>
2024-08-13 23:18:41 -0400

    test: implementação do teste para rota login

--- test/controllers/users_controller.test.js
+++ test/controllers/users_controller.test.js
@@ -1,6 +1,7 @@
 import supertest from 'supertest';
 import app from '../../src/index.js';
 import { truncateUsersTable } from '../../src/database/trucate.js';
+import { pool } from '../../src/database/database.js';
 
 beforeEach(async () => {
     await truncateUsersTable();
@@ -10,24 +11,34 @@ afterEach(async () => {
     await truncateUsersTable();
 });
 
-describe ("POST /usuario/login login", ()=>{
-
-    it('Status 200: A rota deve verificar os dados e liberar o acesso', async ()=>{
-
+describe('POST /usuario/login login', () => {
+    it('Status 200: A rota deve verificar os dados e liberar o acesso', async () => {
         const body = {
             user_name: 'tales',
-            password: '123'            
-        }
+            password: '123',
+        };
 
+        await pool.query(
+            `
+            INSERT INTO users (user_name, password)
+            VALUES ($1, $2);
+        `,
+            [body.user_name, body.password]
+        );
         await supertest(app)
-        .post('/post')
-        .send(body)
-        .expect((res)=>{
-
-            
-
-        })
-
-    })
-
-})
+            .post('/usuario/login')
+            .send(body)
+            .expect((res) => {
+                const { body, status } = res;
+                try {
+                    expect(status).toBe(200);
+                } catch (err) {
+                    throw new Error(`
+                            erro: \n${err}\n
+                            body: \n${body}\n
+                            status: \n${status}\n
+                            `);
+                }
+            });
+    });
+});
