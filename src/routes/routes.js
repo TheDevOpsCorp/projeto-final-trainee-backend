0fb94eedc3b9216ea96b8a94622999daa48c798e
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-08-15 17:17:57 -0300

    chore: add routa post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import postController from '../controllers/post.controller.js';
 
 const  router = Router()
+
+router.post('/createpost', postController.createPost)
 
 
 
