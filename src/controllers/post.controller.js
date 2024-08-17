6ee3a7673e5f98b206f3cd3824a2ad358058da8f
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-08-17 20:07:21 -0300

    chore: add user_id

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -4,9 +4,12 @@ export default {
         // @ts-ignore
     createPost:async(req,res)=>{
         // @ts-ignore
-        let {title,body,date} = req.body
+        let {user_id,title,body,date} = req.body
+        if(!user_id||!title||!body){
+            res.status(400).json({message:"400 : preencha todos os dados"})
+        }
         try {
-            let post = await pool.query('INSERT INTO posts (title,body,created_at) VALUES($1,$2,$3)',[title,body,date])
+            let post = await pool.query('INSERT INTO posts (title,body,user_id,created_at) VALUES($1,$2,$3,$4)',[title,body,user_id,date])
             res.status(201).json({message:"201 : post criado com sucesso"})
         } catch (error) {
             console.error(error)
