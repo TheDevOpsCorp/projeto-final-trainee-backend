d14295d119b1250befc7e393e1c5749229cb4174
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-08-17 20:06:52 -0300

    chore:  add test criando dados e faltando dados

--- src/test/post.test.js
+++ src/test/post.test.js
@@ -1,10 +1,14 @@
+// @ts-ignore
 import supertest from "supertest";
 import app from "../index.js";
 
 
+// @ts-ignore
 describe("POST /post", () => {
-    it("criando dados no bd", async () => {
+    // @ts-ignore
+    it("201:post criado com sucesso", async () => {
       let post = {
+        user_id:2,
         title: "davi",
         body: "lucasemanoel",
         date:"2024-08-15 13:45:00"
@@ -13,15 +17,41 @@ describe("POST /post", () => {
         .post("/createpost")
         .send(post)
         .expect(201)
+        // @ts-ignore
         .expect((res) => {
           try {
             let { message } = res.body;
-            expect(message).toBe("post criado com sucesso");
+            // @ts-ignore
+            expect(message).toBe("201 : post criado com sucesso");
           } catch (error) {
             throw new Error(`error ${res} 
                                    ${error}`);
           }
         });
     });
+
+    // @ts-ignore
+    it("400:faltando dados",async ()=>{
+      let post = {
+        user_id:2,
+        title: "",
+        body: "lucasemanoel",
+        date:"2024-08-15 13:45:00"
+      };
+      await supertest(app)
+        .post("/createpost")
+        .send(post)
+        .expect(400)
+        // @ts-ignore
+        .expect((res) => {
+          let { message } = res.body;
+          // @ts-ignore
+          expect(message).toBe("Preencha todos os dados");
+        });
+
+      
+    })
+
+    
   
   });
