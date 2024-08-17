3ae60e7563275de2dfd3194800f4fc1761a15419
Ruansunshine <ruandasilvasantos49@gmail.com>
2024-08-17 00:58:02 -0300

    chore: subindo primeiro index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import express from "express"
-import helmet from "helmet"
-import cors from "cors"
-import router from "./routes/routes.js"
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import router from "./routes/routes.js";
+import { erroHandler } from "./middleware/auth.err.middleware.js";
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
 //middlewares obrigatorios
-app.use(express.json())
-app.use(helmet())
-app.use(cors())
-
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app;
 //roteador
-app.use('/',router)
+app.use("/", router);
+
+//middlewares tratamento de erros
+app.use(erroHandler);
 
 //entrypoint
-app.listen(port,()=>{
-    console.log("Servidor ativo em: http://localhost:3000")
-})
+app.listen(port, () => {
+  console.log("Servidor ativo em: http://localhost:3000");
+});
 
-
-export default app
+export default app;
