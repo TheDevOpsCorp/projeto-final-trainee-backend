575045d61421c2ad6237f5d9bb18da777d7fd49f
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2024-08-21 17:40:00 -0300

    chore: implementação do error 409

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,9 +17,23 @@ export default {
           .json({ menssage: "400 - Bad request: Body incompleto" });
       }
 
+      const checkSame = await pool.query(
+        `SELECT 1 FROM "users" WHERE username = $1`,
+        [body.name]
+      );
+
+      if (checkSame.rows.length > 0) {
+        return res.status(409).json({
+          menssage:
+            "409 - Conflict: Já existe um usuário com esse nome, escolha um nome diferente.",
+        });
+      }
+
       const hashPassword = await bcrypt.hash(body.password, 10);
+
       const dbRes = await pool.query(
-        `INSERT INTO "users" ("username", "password", "created_at") VALUES ('${body.name}', '${hashPassword}', '${body.creAt}') RETURNING *`
+        'INSERT INTO "users" ("username", "password", "created_at") VALUES ($1, $2, $3) RETURNING *',
+        [body.name, hashPassword, body.creAt]
       );
 
       if (dbRes.rows.length > 0) {
