ba13048af8c36d8795f6f2c901b6147415541170
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2024-08-20 23:52:20 -0300

    chore: configuração das rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import userController from "../controllers/user.controller.js";
 
-const  router = Router()
+const router = Router();
 
+router.post("/user/signin", userController.signIn);
 
-
-export default router
+export default router;
