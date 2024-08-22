3612fea5938cc00088331e87b4f123fa50d512c0
ItzTas <talessabini2@gmail.com>
2024-08-22 19:37:50 -0400

    chore: começando a implementar segundo teste

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,22 +1,31 @@
-import supertest from "supertest";
-import test_app from "./test.router.js";
+import supertest from 'supertest';
+import test_app from './test.router.js';
+import { signJWT } from '../src/helpers/auth.js';
+import { formatObject } from '../src/helpers/general_helpers.js';
 
+describe('AUTH TEST', () => {
+    const path = '/test/middlware/autenticacao';
 
-describe('AUTH TEST',()=>{
-    const path = '/test/middlware/autenticaçao'
+    it('401: token nao veio na autenticacao', async () => {
+        await supertest(test_app)
+            .post(path)
+            .send()
+            .expect((res) => {
+                const { status, body } = res;
+                try {
+                    expect(status).toBe(401);
+                    expect(body).toHaveProperty('error', 'invalido');
+                } catch (error) {
+                    throw new Error(`
+                                 \n${status}\n
+                                 \n${formatObject(body)}\n
+                                 \n${error}\n
+                        `);
+                }
+            });
+    });
 
-    it('401: token nao veio na autenticacao',()=>{
-        supertest(test_app).post(path).send().expect((res)=>{
-            const {status,body} = res
-            try {
-                expect(status).toBe(401)
-                expect(body).toHaveProperty("error","invalido")
-
-            } catch (error) {
-                throw new Error(`${status}
-                                 ${body}
-                                 ${error}`)
-            }
-        })
-    })
-})
+    it('401: token deve expirar quando passar o tempo', async () => {
+        const token = signJWT({ id: 1 }, { expiresIn: '1ms' });
+    });
+});
