e97c1e660be9b8cec1454ccedcc65c221a355577
Ruansunshine <ruandasilvasantos49@gmail.com>
2024-08-22 12:44:03 -0300

    chore: mais uma alteração

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,16 @@
+import 'dotenv/config';
 import pkg from "pg";
 const { Pool } = pkg;
 
 // Configurações de conexão com o banco de dados
 const pool = new Pool({
-  user: "postgres", // Nome do usuário definido no Docker Compose
-  host: "localhost", // O PostgreSQL está rodando localmente
-  database: "postgres", // O nome do banco de dados deve corresponder ao nome do banco inicializado
-  password: "postgres", // Senha definida no Docker Compose
-  port: 5432, // Porta mapeada no Docker Compose
+  user: process.env['DB_USER'],         // Nome do usuário
+  host: process.env['DB_HOST'],         // Host do banco de dados
+  database: process.env['DB_DATABASE'], // Nome do banco de dados
+  password: process.env['DB_PASSWORD'], // Senha do banco de dados
+  port: process.env['DB_PORT'],         // Porta do banco de dados
 });
+
 
 export default pool;
 
