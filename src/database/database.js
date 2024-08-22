336953589ceb3e32b02fdc9d90f865a443ba3ec5
Ruansunshine <ruandasilvasantos49@gmail.com>
2024-08-22 14:35:45 -0300

    chore: alterando

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,16 @@
 import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
-
+//ts @ignore
 // Configurações de conexão com o banco de dados
-const pool = new Pool({
-  user: process.env["POSTGRES_USER"],
-  host: process.env["POSTGRES_HOST"], 
+
+  const pool = new Pool({
+  user: process.env['POSTGRES_USER'],
+  host: process.env['POSTGRES_HOST'], 
+  password: process.env['POSTGRES_PASSWORD'], 
   // @ts-ignore
-  port: process.env["POSTGRES_PORT"],
-  password: process.env["POSTGRES_PASSWORD"], 
-  database: process.env["POSTGRES_DATABASE"], 
+  port: process.env['POSTGRES_PORT'],
+  database: process.env['POSTGRES_DATABASE'], 
  
 });
 
