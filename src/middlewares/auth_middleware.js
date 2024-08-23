48ef2cedc55124445e5a35b27180b1023a608e39
ItzTas <talessabini2@gmail.com>
2024-08-23 19:13:19 -0400

    chore: melhor validação de retornos do pool

--- src/middlewares/auth_middleware.js
+++ src/middlewares/auth_middleware.js
@@ -5,68 +5,76 @@
  */
 
 import {
-  decodeJWT,
-  getToken,
-  JsonWebTokenError,
-  TokenExpiredError,
+    decodeJWT,
+    getToken,
+    JsonWebTokenError,
+    TokenExpiredError,
 } from '../helpers/auth.js';
 import pool from '../database/database.js';
 
 export default {
-  /**
-   *
-   *
-   * @param {Request} req
-   * @param {Response} res
-   * @param {Function} next
-   * @async
-   */
-  // @ts-ignore
-  async authenticateJWT(req, res, next) {
-    let token;
-    try {
-      token = getToken(req);
-    } catch (err) {
-      return res.status(401).json({
-        error: 'invalido',
-      });
-    }
+    /**
+     *
+     *
+     * @param {Request} req
+     * @param {Response} res
+     * @param {Function} next
+     * @async
+     */
+    // @ts-ignore
+    async authenticateJWT(req, res, next) {
+        let token;
+        try {
+            token = getToken(req);
+        } catch (err) {
+            return res.status(401).json({
+                error: 'invalido',
+            });
+        }
 
-    let decoded;
-    try {
-      //@ts-ignore
-      decoded = decodeJWT(token);
-    } catch (err) {
-      if (err instanceof TokenExpiredError) {
-        return res.status(401).json({
-          error: 'expirado',
-        });
-      }
-      if (err instanceof JsonWebTokenError) {
-        return res.status(401).json({
-          error: 'invalido',
-        });
-      }
-      return res.status(500).json({
-        error: 'server error',
-      });
-    }
+        let decoded;
+        try {
+            //@ts-ignore
+            decoded = decodeJWT(token);
+        } catch (err) {
+            if (err instanceof TokenExpiredError) {
+                return res.status(401).json({
+                    error: 'expirado',
+                });
+            }
+            if (err instanceof JsonWebTokenError) {
+                return res.status(401).json({
+                    error: 'invalido',
+                });
+            }
+            return res.status(500).json({
+                error: 'server error',
+            });
+        }
 
-    // req.user = user
-    try {
-      // @ts-ignore
-      let id = decoded.sub;
-      let reuslt = await pool.query('SELECT *FROM users WHERE id = $1 ', [id]);
-      if (reuslt.rows.length == 0) {
-        return res.status(404).json({ error: 'usuario nao encontrado' });
-      }
-      // @ts-ignore
-      req.user = reuslt.rows[0];
-      next();
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ error: 'não foi possivel pegar o usuario', error_infos: err });
-    }
-  },
+        // req.user = user
+        try {
+            // @ts-ignore
+            let id = decoded.sub;
+            let result = await pool.query('SELECT * FROM users WHERE id = $1 ', [id]);
+            if (result.rows.length == 0) {
+                return res.status(404).json({ error: 'usuario nao encontrado' });
+            }
+            if (result.rows.length > 1) {
+                console.log(`ERROR: Mais de um usuário foi encontrado com o id ${id}`);
+                return res
+                    .status(500)
+                    .json({
+                        error: 'Houve um erro no banco de dados ao autenticar o usuário',
+                    });
+            }
+            // @ts-ignore
+            req.user = result.rows[0];
+            next();
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ error: 'não foi possivel pegar o usuario', error_infos: err });
+        }
+    },
 };
